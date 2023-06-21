c5c3f995ab6527174d7fd6ec41324320812baa0e
Wael88He <waelhenawy53@gmail.com>
2023-06-21 19:11:09 +0300

    new3

--- new/settings.py
+++ new/settings.py
@@ -136,14 +136,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-#LANGUAGE_CODE = 'en-us'
+
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
-
+DEFAULT_CHARSET = 'utf-8'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
